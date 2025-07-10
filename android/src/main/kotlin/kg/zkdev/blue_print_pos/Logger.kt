fef78cbf2328fed07299de40b4d3f3d20616b57d
Rizvan Adzhoyan <100145184+radzhoyan@users.noreply.github.com>
2025-07-10 12:52:31 +0600

    update Logger.kt

--- android/src/main/kotlin/kg/zkdev/blue_print_pos/Logger.kt
+++ android/src/main/kotlin/kg/zkdev/blue_print_pos/Logger.kt
@@ -1,12 +1,9 @@
 package kg.zkdev.blue_print_pos
 
 import android.util.Log
-import kg.zkdev.blue_print_pos.BuildConfig
 
 object Logger {
     fun log(message: String) {
-        if (BuildConfig.DEBUG) {
             Log.d("BluePrintPos", message)
-        }
     }
 }
